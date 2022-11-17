3361751f44f8c17b3d880b6cd36b619d05782434
lgvv <lgw9898@naver.com>
2022-11-17 21:30:22 +0900

    [추가] 프로젝트 초기 세팅

---
+++ SmartAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartAI
+//
+//  Created by Hamlit Jason on 2022/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
