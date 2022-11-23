866c7e755aee32263d351379bcc20dda08192524
lgvv <lgw9898@naver.com>
2022-11-23 14:32:27 +0900

    [수정] 바나나 모델 코드 리팩토링

--- SmartAI/Models/ChartInfo.swift
+++ SmartAI/Models/ChartInfo.swift
@@ -1,32 +1,23 @@
-////
-////  ChartInfo.swift
-////  SmartAI
-////
-////  Created by Hamlit Jason on 2022/11/22.
-////
 //
-//import Foundation
+//  ChartInfo.swift
+//  SmartAI
 //
-//class ChartItem: Identifiable {
-//    let id = UUID().uuidString
-//    
-//    var bananaClass: String
-//    var probability: String
-//    
-//    init(bananaClass: String, probability: String) {
-//        self.bananaClass = bananaClass
-//        self.probability = probability
-//    }
-//}
+//  Created by Hamlit Jason on 2022/11/22.
 //
-//class ChartInfo: ObservableObject, Identifiable {
-//    let id = UUID().uuidString
-//    
-//    @Published var localData: [ChartItem] = [] {
-//        didSet { print("🧓🏾 \(localData)")}
-//    }
-//    
-//    @Published var serverData: [ChartItem] = [] {
-//        didSet { print("🧕🏻 \(serverData)")}
-//    }
-//}
+
+import Foundation
+
+// MARK: - 바나나 차트
+struct BananaChartInfo: Identifiable {
+    var id = UUID().uuidString
+    
+    /**바나나 이름 */ var name: String
+    /** 확률 */ var probability: String
+}
+
+struct ChartInfo: Identifiable {
+    let type: String
+    let bananas: [BananaChartInfo]
+    
+    var id: String { type }
+}
--- SmartAI/Scene/Chart/ChartView.swift
+++ SmartAI/Scene/Chart/ChartView.swift
@@ -8,46 +8,6 @@
 import SwiftUI
 import Charts
 
-struct SalesSummary: Identifiable {
-    var id: String = UUID().uuidString
-    
-    var weekday: String
-    var sales: Int
-}
-
-let sfData: [SalesSummary] = [
-    .init(weekday: "안녕", sales: 100),
-    .init(weekday: "가방", sales: 150),
-    .init(weekday: "옷", sales: 50)
-]
-
-let cuData: [SalesSummary] = [
-    .init(weekday: "안녕", sales: 50),
-    .init(weekday: "가방", sales: 100),
-    .init(weekday: "옷", sales: 150)
-]
-
-
-struct Series: Identifiable {
-    let city: String
-    let sales: [SalesSummary]
-    
-    var id: String { city }
-}
-
-let seriesData: [Series] = [
-    .init(city: "다림쥐네 집", sales: sfData),
-    .init(city: "씨유 집", sales: cuData)
-]
-
-// MARK: - 바나나 차트
-struct BananaChartInfo: Identifiable {
-    var id = UUID().uuidString
-    
-    /**바나나 이름 */ var name: String
-    /** 확률 */ var probability: String
-}
-
 let localBananaInfo: [BananaChartInfo] = [
     .init(name: "1", probability: "30"),
     .init(name: "2", probability: "10"),
@@ -61,13 +21,6 @@ let serverBananaInfo: [BananaChartInfo] = [
     .init(name: "3", probability: "40"),
     .init(name: "4", probability: "10")
 ]
-
-struct ChartInfo: Identifiable {
-    let type: String
-    let bananas: [BananaChartInfo]
-    
-    var id: String { type }
-}
 
 struct ChartView: View {
 
